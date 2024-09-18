8574a80e923ed4ffa1abcb5a6fc637b4f65d2df8
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-18 12:44:22 +0200

    seperated into 2 files

--- www/first/index.php
+++ www/first/index.php
@@ -1,99 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$books = [
+    "1984",
+    "Brave New World",
+    "Fahrenheit 451",
+    "The Great Gatsby"
 
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
+];
+
+$assosiative = [
+    "name" => "Bookname",
+    "author" => "Me",
+    "date" => "today"
+];
+
+function filter(array $items): array
+{
+    $filtered = [];
+
+    foreach ($items as $item) {
+        if (str_contains($item, "1")) {
+            $filtered[] = $item;
         }
-    </style>
-</head>
+    }
+    return $filtered;
+}
 
-<body>
-    <div>
-        <h1>Recommended Books</h1>
-        <?php
-        $books = [
-            "1984",
-            "Brave New World",
-            "Fahrenheit 451",
-            "The Great Gatsby"
+function filterGeneric(array $items, callable $filter): array
+{
+    $filtered = [];
 
-        ];
-
-        $assosiative = [
-            "name" => "Bookname",
-            "author" => "Me",
-            "date" => "today"
-        ];
-
-        function filter(array $items): array
-        {
-            $filtered = [];
-
-            foreach ($items as $item) {
-                if (str_contains($item, "1")) {
-                    $filtered[] = $item;
-                }
-            }
-            return $filtered;
+    foreach ($items as $item) {
+        if ($filter($item)) {
+            $filtered[] = $item;
         }
+    }
+    return $filtered;
+}
 
-        function filterGeneric(array $items, callable $filter): array
-        {
-            $filtered = [];
-
-            foreach ($items as $item) {
-                if ($filter($item)) {
-                    $filtered[] = $item;
-                }
-            }
-            return $filtered;
-        }
-        ?>
-
-
-        <ul>
-            <?php foreach ($books as $book) {
-                echo "<li>{$book}</li>";
-            } ?>
-        </ul>
-
-        Another way to display it:
-        <ul>
-            <?php foreach ($books as $book): ?>
-                <li><?= $book ?></li>
-            <?php endforeach ?>
-        </ul>
-
-
-        And we can display assosiative Arrays like this: <br>
-        <ul>
-            <li> <?= $assosiative["name"] ?> </li>
-            <li> <?= $assosiative["author"] ?> </li>
-            <li> <?= $assosiative["date"] ?> </li>
-        </ul>
-
-        And here are all Books that have a 1 in there title: <br>
-        <ul>
-            <?php foreach (filter($books) as $book): ?>
-                <li><?= $book ?></li>
-            <?php endforeach ?>
-        </ul>
-
-        All books that have and e in the title:
-        <ul>
-            <?php foreach (filterGeneric($books, fn($i) => str_contains($i, "e")) as $book): ?>
-                <li><?= $book ?></li>
-            <?php endforeach ?>
-        </ul>
-    </div>
-</body>
-
-</html>
+require "index.view.php";
---
+++ www/first/index.view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <h1>Recommended Books</h1>
+
+        <ul>
+            <?php foreach ($books as $book) {
+                echo "<li>{$book}</li>";
+            } ?>
+        </ul>
+
+        Another way to display it:
+        <ul>
+            <?php foreach ($books as $book): ?>
+                <li><?= $book ?></li>
+            <?php endforeach ?>
+        </ul>
+
+
+        And we can display assosiative Arrays like this: <br>
+        <ul>
+            <li> <?= $assosiative["name"] ?> </li>
+            <li> <?= $assosiative["author"] ?> </li>
+            <li> <?= $assosiative["date"] ?> </li>
+        </ul>
+
+        And here are all Books that have a 1 in there title: <br>
+        <ul>
+            <?php foreach (filter($books) as $book): ?>
+                <li><?= $book ?></li>
+            <?php endforeach ?>
+        </ul>
+
+        All books that have and e in the title:
+        <ul>
+            <?php foreach (filterGeneric($books, fn($i) => str_contains($i, "e")) as $book): ?>
+                <li><?= $book ?></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</body>
+
+</html>
