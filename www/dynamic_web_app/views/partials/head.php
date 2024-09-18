f1dfe8a45bf4b4829484b48b83e1500a7785e460
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-18 15:25:26 +0200

    partial views

---
+++ www/dynamic_web_app/views/partials/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?= $heading?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="min-h-full"></div>
