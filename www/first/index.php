fa97b405e43f8fe0a4e46e97c0c161a981cc2f62
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-18 11:51:02 +0200

    assosiative arrays

--- www/first/index.php
+++ www/first/index.php
@@ -24,19 +24,35 @@
             "Brave New World",
             "Fahrenheit 451",
             "The Great Gatsby"
+
         ];
+
+        $assosiative = [
+            "name" => "Bookname",
+            "author" => "Me",
+            "date" => "today"
+        ];
+
         ?>
         <ul>
             <?php foreach ($books as $book) {
                 echo "<li>{$book}</li>";
             } ?>
         </ul>
+
         Another way to display it:
         <ul>
             <?php foreach ($books as $book): ?>
                 <li><?= $book ?></li>
             <?php endforeach ?>
         </ul>
+        
+
+        And we can display assosiative Arrays like this: <br>
+        <?= $assosiative["name"] ?> <br>
+        <?= $assosiative["author"] ?> <br>
+        <?= $assosiative["date"] ?> <br>
+        
     </div>
 </body>
 
