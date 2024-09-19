144854101f14fba2b1f2a2cad6bd56e54d51f1fb
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-19 21:43:01 +0200

    fixed header button

--- www/dynamic_web_app/views/partials/nav.php
+++ www/dynamic_web_app/views/partials/nav.php
@@ -8,11 +8,11 @@
                 <div class="md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="./" class=" text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'index.php') ?
+                        <a href="./" class=" text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'index') || str_ends_with($_SERVER['REQUEST_URI'], "/") ?
                             'bg-gray-900 text-white' : '' ?> rounded-md  px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Home</a>
-                        <a href="about" class=" text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'about.php') ?
+                        <a href="about" class=" text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'about') ?
                             'bg-gray-900 text-white' : '' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="contact" class="text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'contact.php') ?
+                        <a href="contact" class="text-gray-300 <?= str_contains($_SERVER['REQUEST_URI'], 'contact') ?
                             'bg-gray-900 text-white' : '' ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
