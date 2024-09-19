e221d18dcca91d9cd245f5276d3284145995d180
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-19 16:37:06 +0200

    Slight redesign

--- www/dynamic_web_app/views/index.view.php
+++ www/dynamic_web_app/views/index.view.php
@@ -4,26 +4,28 @@
 <?php require "partials/banner.php" ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        Home page with cool text
+        <h2 class="py-1 px-2 min-w-full text-xl font-bold"> Here is a list of all blog posts in the Database: </h2>
+
+        <div>
+            <table class="table-auto min-w-full px-3 py-1">
+                <thead class="border-b">
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($db->fetchAll("select * from posts") as $post): ?>
+                        <tr class="border-b">
+                            <td class="px-3 py-1 border-r border-l text-center"><?= $post['id'] ?></td>
+                            <td class="px-3 py-1 border-r border-l"><?= $post['title'] ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
-    <div>
-        <table class="table-auto min-w-full px-3 py-1">
-            <thead class="border-b">
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($db->fetchAll("select * from posts") as $post): ?>
-                    <tr class="border-b">
-                        <td class="px-3 py-1 border-r border-l text-center"><?= $post['id'] ?></td>
-                        <td class="px-3 py-1 border-r border-l"><?= $post['title'] ?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
-    </div>
+
 </main>
 <?php require "partials/footer.php" ?>
