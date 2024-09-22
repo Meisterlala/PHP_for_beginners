5e94965065301e7f2d33d4fbc76a8c15ddce13fa
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-09-22 16:22:09 +0200

    changed for the index

--- www/notes/routes.php
+++ www/notes/routes.php
@@ -2,8 +2,9 @@
 
 
 $router->get('/', 'controllers/index.php');
+$router->get('/index.php', 'controllers/index.php');
 $router->get('/note', 'controllers/note.php');
-$router->delete('/note', 'controllers/destroy.php'); 
+$router->delete('/note', 'controllers/destroy.php');
 $router->patch('/note', 'controllers/update.php');
 $router->post('/note', 'controllers/store.php');
 $router->get('/edit', 'controllers/edit.php');
